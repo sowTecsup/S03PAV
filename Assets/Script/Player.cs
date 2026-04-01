2b679fdc81fede3e6f7067db0f6608f0a2ec75a6
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-01 16:17:49 -0500

    AutoCombat

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,24 +4,45 @@ public class Player : MonoBehaviour
 {
     public BaseStats stats;
 
+    public float range;
+
 
     private void Awake()
     {
         stats = new BaseStats(10, 10, 5, 1, 20);//->A
         print(stats.Power);
 
-//        stats.Power = 1;
+
         
     }
     void Start()
     {
-      //  Destroy(this.gameObject);
+        InvokeRepeating("AutoAttackEnemies",1f, 1f);
     }
 
     void Update()
     {
         
     }
+
+
+
+    public void AutoAttackEnemies()
+    {
+        print("ATAQUE!");
+
+        GameObject[] allEnemies =  GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in allEnemies)
+        {
+            float distance = Vector3.Distance(enemy.transform.position, transform.position);
+
+            if(distance <= range)
+                enemy.GetComponent<Enemy>().TakeDamage(this);
+        }
+
+    }
+
     private void OnDestroy()
     {
         Debug.Log("oh no me cancelaron");
